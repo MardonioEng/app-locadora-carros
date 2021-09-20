bd447d7cccc6b542666e0348dff165154774b974
MardonioEng <mardoniorodrigues11@gmail.com>
2021-09-20 15:38:49 -0300

    Aplicando filtros nas buscas de modelo e marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -20,9 +20,33 @@ class MarcaController extends Controller
      *
      * @return JsonResponse
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
-        $marcas = $this->marca->with('modelos')->get();
+        $marcas = array();
+
+        if($request->has('atributos_modelos')) {
+            $atributos_modelos = $request->atributos_modelos;
+            $marcas = $this->marca->with('modelos:id,'.$atributos_modelos);
+        }else {
+            $marcas = $this->marca->with('modelos');
+        }
+
+        if($request->has('filtro')) {
+            $filtros = explode(';', $request->filtro);
+            foreach ($filtros as $key => $condicao) {
+                $c = explode(':', $condicao);
+                $marcas = $marcas->where($c[0], $c[1], $c[2]);
+            }
+        }
+
+        if($request->has('atributos')) {
+            $atributos = $request->atributos;
+            $marcas = $marcas->selectRaw($atributos)->get();
+        }else {
+            $marcas = $marcas->get();
+        }
+
+        //$marcas = $this->marca->with('modelos')->get();
         return response()->json($marcas, 200);
     }
 
